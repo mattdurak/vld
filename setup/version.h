3d41e757171d25967313bb7c361cd45f1543a39d
Matt Durak <mdurak@microsoft.com>
2020-10-16 09:05:54 -0700

    Make initterm callstack fix to try to stop false positives

--- setup/version.h
+++ setup/version.h
@@ -1,9 +1,9 @@
 
-#define VLDVERSION          L"2.5.4"
-#define VERSION_NUMBER		2,5,4,0
-#define VERSION_STRING		"2.5.4.0"
+#define VLDVERSION          L"2.5.5"
+#define VERSION_NUMBER		2,5,5,0
+#define VERSION_STRING		"2.5.5.0"
 #define VERSION_COPYRIGHT	"Copyright (C) 2005-2020"
 
 #ifndef __FILE__
-!define VLD_VERSION "2.5.4"	// NSIS Script
+!define VLD_VERSION "2.5.5"	// NSIS Script
 #endif
